125c787b3afd191d2e6cc65ce4dd2bbe4fbc6471
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2025-02-07 23:39:28 +0200

    add calculate net profit class to hold function to calc net profit

--- src/main/java/com/gamingCoffee/utiles/CalculateNetProfit.java
+++ src/main/java/com/gamingCoffee/utiles/CalculateNetProfit.java
@@ -1,4 +1,8 @@
 package com.gamingCoffee.utiles;
 
 public class CalculateNetProfit {
+
+  public static double calcResult(double income, double expense) {
+    return Math.round(income - expense) ;
+  }
 }
