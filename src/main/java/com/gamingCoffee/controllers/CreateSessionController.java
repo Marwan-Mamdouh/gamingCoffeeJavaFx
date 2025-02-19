d7e86990ec11b0b8c1c7abbe87a7bef459d64ddf
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2025-02-19 21:10:52 +0200

    change spotNumberField to chiocebox field

--- src/main/java/com/gamingCoffee/controllers/CreateSessionController.java
+++ src/main/java/com/gamingCoffee/controllers/CreateSessionController.java
@@ -1,18 +1,24 @@
 package com.gamingCoffee.controllers;
 
 import com.gamingCoffee.services.SessionService;
+import com.gamingCoffee.services.SpotService;
 import com.gamingCoffee.utiles.PopupUtil;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class CreateSessionController {
+public class CreateSessionController implements Initializable {
 
   // field
   @FXML
-  private TextField spotNumberFiled;
+  private ChoiceBox<String> spotNumberChoiceBox;
   @FXML
   private TextField noControllersFiled;
   @FXML
@@ -32,7 +38,7 @@ public class CreateSessionController {
   @FXML
   void createSessionAction() {
     try {
-      if (SessionService.createSession(Integer.parseInt(spotNumberFiled.getText()),
+      if (SessionService.createSession(Integer.parseInt(spotNumberChoiceBox.getValue()),
           Integer.parseInt(noControllersFiled.getText()), endTimeFiled.getText())) {
         PopupUtil.showPopup("Success", "Session Created.", AlertType.INFORMATION);
         cancelAction();
@@ -41,6 +47,21 @@ public class CreateSessionController {
       }
     } catch (NumberFormatException e) {
       PopupUtil.showErrorPopup(e);
+    }
+  }
+
+  /**
+   * @param location  The location used to resolve relative paths for the root object, or
+   *                  {@code null} if the location is not known.
+   * @param resources The resources used to localize the root object, or {@code null} if the root
+   *                  object was not localized.
+   */
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    try {
+      spotNumberChoiceBox.getItems().addAll(SpotService.getFreeSpotsNumbers());
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
     }
   }
 }
