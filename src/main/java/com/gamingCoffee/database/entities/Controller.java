623da00e07b69616df4c5df3564b30d884b609e2
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2025-03-08 22:20:10 +0200

    change from normal final class to record

--- src/main/java/com/gamingCoffee/database/entities/Controller.java
+++ src/main/java/com/gamingCoffee/database/entities/Controller.java
@@ -1,74 +1,27 @@
 package com.gamingCoffee.database.entities;
 
 import com.gamingCoffee.models.ControllerType;
-import java.util.Objects;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-public class Controller {
+public record Controller(int controllerId, ControllerType controllerType) {
 
-  // Required fields
-  private final int controllerId;
-  private final ControllerType controllerType;
-
-  private Controller(Builder builder) {
-    this.controllerId = builder.controllerId;
-    this.controllerType = builder.controllerType;
+  public Controller {
+    if (controllerId <= 0) {
+      throw new IllegalArgumentException("Controller ID Couldn't be less than 0.");
+    }
   }
 
-  public ControllerType getControllerType() {
-    return controllerType;
+  @Contract("_, _ -> new")
+  public static @NotNull Controller with(int controllerId, ControllerType controllerType) {
+    return new Controller(controllerId, controllerType);
   }
 
   public int getControllerId() {
     return controllerId;
   }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
-    }
-    Controller that = (Controller) o;
-    return controllerId == that.controllerId && controllerType == that.controllerType;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(controllerId, controllerType);
-  }
-
-  @Override
-  public String toString() {
-    return String.format("Controller{controllerId=%d, controllerType=%s}", controllerId,
-        controllerType);
-  }
-
-  public static class Builder {
-
-    // Required fields
-    private ControllerType controllerType = null;
-    private int controllerId = 0;
-
-    // Constructor for required fields
-    public Builder controllerType(ControllerType controllerType) {
-      this.controllerType = controllerType;
-      return this;
-    }
-
-    // Method to set optional field
-    public Builder controllerId(int controllerId) {
-      if (controllerId <= 0) {
-        throw new IllegalArgumentException("Controller ID must be positive.");
-      }
-      this.controllerId = controllerId;
-      return this;
-    }
-
-    // Build method to create the Controller object
-    public Controller build() {
-      return new Controller(this);
-    }
+  public ControllerType getControllerType() {
+    return controllerType;
   }
 }
