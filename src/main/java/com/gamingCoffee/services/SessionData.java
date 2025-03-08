71494c80231c06b9e1d31b56adffcae66ad4e6bb
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2025-03-08 22:05:12 +0200

    minor changes

--- src/main/java/com/gamingCoffee/services/SessionData.java
+++ src/main/java/com/gamingCoffee/services/SessionData.java
@@ -3,7 +3,7 @@ package com.gamingCoffee.services;
 import com.gamingCoffee.models.ConsoleType;
 import com.gamingCoffee.models.SpotType;
 
-public record SessionData(int sessionId, int noController, double duration,
-                          SpotType spotType, ConsoleType consoleType) {
+public record SessionData(int sessionId, int noController, double duration, SpotType spotType,
+                          ConsoleType consoleType) {
 
 }
