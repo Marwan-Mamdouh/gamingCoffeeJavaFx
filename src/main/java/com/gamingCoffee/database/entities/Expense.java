09b8d5e767229f8f184be5da78475e969d639c35
Marwan-Mamdouh <marwanabdelmajjed@gmail.com>
2025-03-08 22:58:24 +0200

    remove the builder pattern and change it from final class to record

--- src/main/java/com/gamingCoffee/database/entities/Expense.java
+++ src/main/java/com/gamingCoffee/database/entities/Expense.java
@@ -1,22 +1,25 @@
 package com.gamingCoffee.database.entities;
 
 import java.time.LocalDate;
-import java.util.Objects;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
-public class Expense {
+public record Expense(int expenseId, String creator, double expenseAmount, LocalDate expenseDate,
+                      String note) {
 
-  private final int expenseId;
-  private final String creator;
-  private final double expenseAmount;
-  private final LocalDate expenseDate;
-  private final String note;
+  public Expense {
+    validate(expenseId, creator, expenseAmount, expenseDate);
+  }
 
-  private Expense(Builder builder) {
-    this.expenseId = builder.expenseId;
-    this.creator = builder.creator;
-    this.expenseAmount = builder.expenseAmount;
-    this.expenseDate = builder.expenseDate;
-    this.note = builder.note;
+  @Contract("_, _, _ -> new")
+  public static @NotNull Expense create(String creator, double expenseAmount, String note) {
+    return new Expense(1, creator, expenseAmount, null, note);
+  }
+
+  @Contract("_, _, _, _, _ -> new")
+  public static @NotNull Expense read(int expenseId, String creator, double expenseAmount,
+      LocalDate date, String note) {
+    return new Expense(expenseId, creator, expenseAmount, date, note);
   }
 
   public int getExpenseId() {
@@ -39,77 +42,19 @@ public class Expense {
     return note;
   }
 
-  @Override
-  public String toString() {
-    return "Expense{" + "expenseId=" + expenseId + ", creator='" + creator + "', expenseAmount="
-        + expenseAmount + ", expenseDate=" + expenseDate + ", note='" + note + "'}";
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+  private static void validate(int expenseId, String creator, double expenseAmount,
+      LocalDate date) {
+    if (expenseId <= 0) {
+      throw new IllegalArgumentException("Expense ID must be more than 0.");
     }
-    Expense expense = (Expense) o;
-    return getExpenseId() == expense.getExpenseId()
-        && Double.compare(getExpenseAmount(), expense.getExpenseAmount()) == 0 && Objects.equals(
-        getCreator(), expense.getCreator()) && Objects.equals(getExpenseDate(),
-        expense.getExpenseDate()) && Objects.equals(getNote(), expense.getNote());
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(getExpenseId(), getCreator(), getExpenseAmount(), getExpenseDate(),
-        getNote());
-  }
-
-  public static class Builder {
-
-    private int expenseId = 0;
-    private String creator = null;
-    private double expenseAmount = 0.0;
-    private LocalDate expenseDate = null;
-    private String note = null;
-
-    public Builder expenseId(int expenseId) {
-      if (expenseId <= 0) {
-        throw new IllegalArgumentException("Expense ID must be more than 0.");
-      }
-      this.expenseId = expenseId;
-      return this;
+    if (creator == null || creator.isBlank()) {
+      throw new IllegalArgumentException("Creator cannot be null or blank.");
     }
-
-    public Builder creator(String creator) {
-      if (creator == null || creator.isBlank()) {
-        throw new IllegalArgumentException("creator cannot be null or blank.");
-      }
-      this.creator = creator;
-      return this;
+    if (expenseAmount <= 0) {
+      throw new IllegalArgumentException("Amount must be positive.");
     }
-
-    public Builder expenseAmount(double amount) {
-      if (amount <= 0) {
-        throw new IllegalArgumentException("Amount must be Positive.");
-      }
-      this.expenseAmount = amount;
-      return this;
-    }
-
-    public Builder expenseDate(LocalDate date) {
-      if (date == null) {
-        throw new IllegalArgumentException("expenseDate cannot be null.");
-      }
-      this.expenseDate = date;
-      return this;
-    }
-
-    public Builder note(String note) {
-      this.note = note;
-      return this;
-    }
-
-    public Expense build() {
-      return new Expense(this);
+    if (date == null) {
+      throw new IllegalArgumentException("Expense date cannot be null.");
     }
   }
 }
